bb7d09e8b239af928fef7b2a3b90c2cd409b0386
omervavd3 <omervavd3@gmail.com>
2023-03-11 16:52:00 +0200

    Cart

--- orderPage/dist/control.js
+++ orderPage/dist/control.js
@@ -5,7 +5,7 @@ function renderOrders(cart) {
             var itemName = _a[0], item = _a[1];
             debugger;
             html_1 +=
-                "   <br/><br/>\n                    <div class=\"orders__products\">\n                    <div class=\"orders__products__img\">\n                        <img src=" + item.imgLink + " alt=\"\">\n                    </div>\n                    <div class=\"orders__products__name\">" + itemName + "</div>\n                    <div class=\"orders__products__price\">" + item.price + "</div>\n                    <div id=" + item.uid + " class=\"orders__products__quantity\">" + item.quantity + "</div>\n                    <div class=\"orders__products__wrapperBtn\">\n                        <div onclick=\"addQuantity('" + item.uid + "')\" class=\"orders__products__wrapperBtn__btnAdd\">+</div>\n                        <div onclick=\"deleteQuantity('" + item.uid + "')\" class=\"orders__products__wrapperBtn__btnRemove\">-</div>\n                    </div>\n                </div>\n            ";
+                "   <br/><br/>\n                    <div class=\"orders__products\">\n                    <div class=\"orders__products__img\">\n                        <img src=" + item.imgLink + " alt=\"\">\n                    </div>\n                    <div class=\"orders__products__name\">" + itemName + "</div>\n                    <div class=\"orders__products__price\">" + item.price + "</div>\n                    <div id=" + item.uid + " class=\"orders__products__quantity\">" + item.quantity + "</div>\n                    <div class=\"orders__products__wrapperBtn\">\n                        <div onclick=\"addQuantity('" + item.uid + "', '" + itemName + "')\" class=\"orders__products__wrapperBtn__btnAdd\">+</div>\n                        <div onclick=\"deleteQuantity('" + item.uid + "', '" + itemName + "')\" class=\"orders__products__wrapperBtn__btnRemove\">-</div>\n                    </div>\n                </div>\n            ";
         });
         html_1 += "</div>";
         return html_1;
@@ -15,21 +15,35 @@ function renderOrders(cart) {
         return "";
     }
 }
-function addQuantity(itemName) {
-    var quantityElement = document.getElementById(itemName);
+function addQuantity(uid, itemName) {
+    var quantityElement = document.getElementById(uid);
     if (quantityElement !== null) {
         var quantity = Number(quantityElement === null || quantityElement === void 0 ? void 0 : quantityElement.innerText);
         quantity++;
         quantityElement.innerText = String(quantity);
+        var cartStr = localStorage.getItem('cart');
+        if (cartStr) {
+            var cartObj = JSON.parse(cartStr);
+            cartObj[itemName].quantity = quantity;
+            var cartData = JSON.stringify(cartObj);
+            localStorage.setItem('cart', cartData);
+        }
     }
 }
-function deleteQuantity(itemName) {
-    var quantityElement = document.getElementById(itemName);
+function deleteQuantity(uid, itemName) {
+    var quantityElement = document.getElementById(uid);
     if (quantityElement !== null) {
         var quantity = Number(quantityElement === null || quantityElement === void 0 ? void 0 : quantityElement.innerText);
         quantity--;
         if (quantity >= 0) {
             quantityElement.innerText = String(quantity);
+        }
+        var cartStr = localStorage.getItem('cart');
+        if (cartStr) {
+            var cartObj = JSON.parse(cartStr);
+            cartObj[itemName].quantity = quantity;
+            var cartData = JSON.stringify(cartObj);
+            localStorage.setItem('cart', cartData);
         }
     }
 }
